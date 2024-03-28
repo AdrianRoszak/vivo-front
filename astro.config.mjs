01e7a4b50c36fc4f22eeebd22617b30479d99db3
Weirdo <adroszak@weirdo.codes>
2024-03-28 23:12:52 +0100

    setup astro with sanity

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,20 @@
 import { defineConfig } from 'astro/config';
+import sanity from "astro-sanity";
 
-import sanity from "@sanity/astro";
+import { loadEnv } from "vite";
+
+const {
+  PUBLIC_SANITY_PROJECT_ID,
+  PUBLIC_SANITY_API_VERSION,
+  PUBLIC_SANITY_DATASET,
+} = loadEnv(import.meta.env.MODE, process.cwd(), "");
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [sanity()]
+  integrations: [sanity({
+    projectId: PUBLIC_SANITY_PROJECT_ID,
+    dataset: PUBLIC_SANITY_DATASET,
+    apiVersion: PUBLIC_SANITY_API_VERSION,
+    useCdn: true,
+  })]
 });
